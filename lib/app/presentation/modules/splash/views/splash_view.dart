c9e6888677de024d6b2cea964de944bbe36287e4
jocnn <ijann_@outlook.com>
2024-09-24 20:58:09 -0300

    injecting dependence to verify Internet connection

--- lib/app/presentation/modules/splash/views/splash_view.dart
+++ lib/app/presentation/modules/splash/views/splash_view.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+import '../../../../../main.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _init(),
+    );
+  }
+
+  Future<void> _init() async {
+    final connectivityRepo = Injector.of(context).connectivityRepository;
+    final hasInternet = await connectivityRepo.hasInternet;
+
+    if (hasInternet) {
+      debugPrint("🔥");
+    } else {
+      debugPrint("😭");
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'app/data/repositories_implementation/authentication_repository_impl.dart';
+import 'app/data/repositories_implementation/connectivity_repository_imp.dart';
+import 'app/domain/repositories/authentication_repository.dart';
+import 'app/domain/repositories/connectivity_repository.dart';
 import 'app/my_app.dart';
 
 void main() {
   runApp(
-    const MyApp(),
+    Injector(
+      connectivityRepository: ConnectivityRepImp(),
+      authenticationRepository: AuthenticationRepositoryImpl(),
+      child: const MyApp(),
+    ),
   );
+}
+
+class Injector extends InheritedWidget {
+  const Injector({
+    super.key,
+    required this.connectivityRepository,
+    required this.authenticationRepository,
+    required super.child,
+  });
+
+  final ConnectivityRepository connectivityRepository;
+  final AuthenticationRepository authenticationRepository;
+
+  @override
+  bool updateShouldNotify(oldWidget) => false;
+
+  static Injector of(BuildContext context) {
+    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
+    assert(injector != null, 'Injector could no be found');
+    return injector!;
+  }
 }
