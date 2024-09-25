9b4c10aca2667adb8672c5148efc29a57dd016cb
jocnn <ijann_@outlook.com>
2024-09-25 10:40:56 -0300

    User data can be void

--- lib/app/data/repositories_implementation/authentication_repository_impl.dart
+++ lib/app/data/repositories_implementation/authentication_repository_impl.dart
@@ -3,7 +3,7 @@ import '../../domain/repositories/authentication_repository.dart';
 
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
   @override
-  Future<User> getUserData() => Future.value(User());
+  Future<User?> getUserData() => Future.value(User());
 
   @override
   Future<bool> get isSignedIn => Future.value(true);
