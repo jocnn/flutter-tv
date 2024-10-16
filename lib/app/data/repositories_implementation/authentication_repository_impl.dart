9130c0dc46679a08839fd2e565390c243e63243b
jocnn <ijann_@outlook.com>
2024-10-16 11:52:27 -0300

    Defining the function isSignedIn

--- lib/app/data/repositories_implementation/authentication_repository_impl.dart
+++ lib/app/data/repositories_implementation/authentication_repository_impl.dart
@@ -1,10 +1,21 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import '../../domain/models/user.dart';
 import '../../domain/repositories/authentication_repository.dart';
 
+const _key = 'sessionId';
+
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
+  final FlutterSecureStorage _secureStorage;
+
+  AuthenticationRepositoryImpl(this._secureStorage);
+
   @override
   Future<User?> getUserData() => Future.value(User());
 
   @override
-  Future<bool> get isSignedIn => Future.value(true);
+  Future<bool> get isSignedIn async {
+    final sessionId = await _secureStorage.read(key: _key);
+    return sessionId != null;
+  }
 }
