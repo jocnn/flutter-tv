4813a7ad426846d7b5ef5e29af4e5e81020d16de
jocnn <ijann_@outlook.com>
2024-10-19 11:44:10 -0300

    The start of session was defined and implemented

--- lib/app/data/repositories_implementation/authentication_repository_impl.dart
+++ lib/app/data/repositories_implementation/authentication_repository_impl.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import '../../domain/either.dart';
+import '../../domain/enums.dart';
 import '../../domain/models/user.dart';
 import '../../domain/repositories/authentication_repository.dart';
 
@@ -17,5 +19,22 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
   Future<bool> get isSignedIn async {
     final sessionId = await _secureStorage.read(key: _key);
     return sessionId != null;
+  }
+
+  @override
+  Future<Either<SignInFailure, User>> signIn(
+    String username,
+    String password,
+  ) async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    if (username != 'test') {
+      return Either.left(SignInFailure.notFound);
+    }
+    if (password != '123456') {
+      return Either.left(SignInFailure.unAuthorized);
+    }
+    return Either.right(User());
   }
 }
