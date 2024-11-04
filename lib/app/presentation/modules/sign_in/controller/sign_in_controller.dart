deee479b6644e798ac5972b758045cd11ba37556
jocnn <ijann_@outlook.com>
2024-11-04 13:19:15 -0300

    Dispose was added to the controller

--- lib/app/presentation/modules/sign_in/controller/sign_in_controller.dart
+++ lib/app/presentation/modules/sign_in/controller/sign_in_controller.dart
@@ -4,10 +4,12 @@ class SignInController extends ChangeNotifier {
   String _username = '';
   String _password = '';
   bool _fetching = false;
+  bool _mounted = true;
 
   String get username => _username;
   String get password => _password;
   bool get fetching => _fetching;
+  bool get mounted => _mounted;
 
   void onUsernameChanged(String value) {
     _username = value.trim().toLowerCase();
@@ -20,5 +22,11 @@ class SignInController extends ChangeNotifier {
   void onFetchingChanged(bool value) {
     _fetching = value;
     notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _mounted = false;
+    super.dispose();
   }
 }
