bcbe523329318efbed5208dcd503baaa5d75c286
jocnn <ijann_@outlook.com>
2024-11-04 17:30:42 -0300

    Defined the immutable state and method copyWith

---
+++ lib/app/presentation/modules/sign_in/controller/sign_in_state.dart
@@ -0,0 +1,23 @@
+class SignInState {
+  SignInState({
+    this.username = '',
+    this.password = '',
+    this.fetching = false,
+  });
+
+  final String username;
+  final String password;
+  final bool fetching;
+
+  SignInState copyWith({
+    String? username,
+    String? password,
+    bool? fetching,
+  }) {
+    return SignInState(
+      username: username ?? this.username,
+      password: password ?? this.password,
+      fetching: fetching ?? this.fetching,
+    );
+  }
+}
